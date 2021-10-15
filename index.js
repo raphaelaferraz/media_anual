7ace62e2bf2cde5f84df2405f7088c53e008fe49
raphaelaferraz <raphaelagferraz2@gmail.com>
2021-10-15 19:07:07 -0300

    Realizando a verificação do preenchimento dos inputs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Calculo da media do aluno:
 
-function CalculoDaMedia(){
+function CalculoDaMedia() {
     const estruturaDoInputNota1 = document.getElementById("nota1");
     const valorDoInputNota1 = estruturaDoInputNota1.value;
     const nota1 = parseFloat(valorDoInputNota1);
@@ -18,24 +18,47 @@ function CalculoDaMedia(){
     const nota4 = parseFloat(valorDoInputNota4);
 
     /* Só deve realizar o calculo se todos os campos estão preenchidos */
-
-    const calculoDamedia = (nota1 + nota2 + nota3 + nota4)/4;
-    const mediaFixada = calculoDamedia.toFixed(1);
-
+    if (document.getElementById("nota1").value == "" ||
+        document.getElementById("nota2").value == "" ||
+        document.getElementById("nota3").value == "" ||
+        document.getElementById("nota4").value == "") {
+        alert("O cáculo não foi realizado, pois é necessário preencher todos os campos!");
+    }
+    
+    const calculoDamedia = (nota1 + nota2 + nota3 + nota4) / 4;
+    const mediaFixada = calculoDamedia.toFixed(1); 
     const estruturaDoResultado = document.getElementById("resultado");
+    const resultado = estruturaDoResultado;
 
-    if(mediaFixada >= 6){
+    const inputs = {
+        inputNota1: document.getElementById("nota1"),
+        inputNota2: document.getElementById("nota2"),
+        inputNota3: document.getElementById("nota3"),
+        inputNota4: document.getElementById("nota4")
+    }
+
+    //verificação do valor total da media e do preenchimento dos inputs
+    if (mediaFixada >= 6 &&  inputs.inputNota1 != "" 
+        && inputs.inputNota2 != "" && 
+        inputs.inputNota3 != "" && 
+        inputs.inputNota4 != "") {
         estruturaDoResultado.innerHTML = `Parabéns, você foi aprovado! Sua média é de ${mediaFixada}`;
         resultado.style.padding = "16px";
-    }else{
+    }else if(mediaFixada <= 6 &&  inputs.inputNota1 != "" 
+    && inputs.inputNota2 != "" && 
+    inputs.inputNota3 != "" && 
+    inputs.inputNota4 != ""){
         estruturaDoResultado.innerHTML = `Infelizmente, você foi reprovado :( Sua média é de ${mediaFixada}`;
+        resultado.style.padding = "16px";
+    }else {
+        estruturaDoResultado.innerHTML = `Por favor, preencha todos os campos!`
         resultado.style.padding = "16px";
     }
 }
 
 //adiciona ao botão a opção de pressionar a tecla "enter" e executar o cáculo
-document.addEventListener("keypress", function(event){
-    if(event.key === 'Enter'){
+document.addEventListener("keypress", function (event) {
+    if (event.key === 'Enter') {
         const inputEviarEnter = document.querySelector("#input_enviar");
         inputEviarEnter.click();
     }
