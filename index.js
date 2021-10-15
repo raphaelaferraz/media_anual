008ab9cd2e76d5345471e3233c46268ecafa3d80
raphaelaferraz <raphaelagferraz2@gmail.com>
2021-10-14 21:06:11 -0300

    Estruturando o HTML, CSS e JS

---
+++ index.js
@@ -0,0 +1,33 @@
+//Calculo da media do aluno
+
+function CalculoDaMedia(){
+    const estruturaDoInputNota1 = document.getElementById("nota1");
+    const valorDoInputNota1 = estruturaDoInputNota1.value;
+    const nota1 = parseFloat(valorDoInputNota1);
+
+    const estruturaDoInputNota2 = document.getElementById("nota2");
+    const valorDoInputNota2 = estruturaDoInputNota2.value;
+    const nota2 = parseFloat(valorDoInputNota2);
+
+    const estruturaDoInputNota3 = document.getElementById("nota3");
+    const valorDoInputNota3 = estruturaDoInputNota3.value;
+    const nota3 = parseFloat(valorDoInputNota3);
+
+    const estruturaDoInputNota4 = document.getElementById("nota4");
+    const valorDoInputNota4 = estruturaDoInputNota4.value;
+    const nota4 = parseFloat(valorDoInputNota4);
+
+
+    const calculoDamedia = (nota1 + nota2 + nota3 + nota4)/4;
+    const mediaFixada = calculoDamedia.toFixed(1);
+
+    const estruturaDoResultado = document.getElementById("resultado");
+
+    if(mediaFixada >= 6){
+        estruturaDoResultado.innerHTML = `Parabéns, você foi aprovado! Sua média é de ${mediaFixada}`;
+        resultado.style.padding = "16px";
+    }else{
+        estruturaDoResultado.innerHTML = `Infelizmente, você foi reprovado :( Sua média é de ${mediaFixada}`;
+        resultado.style.padding = "16px";
+    }
+}
